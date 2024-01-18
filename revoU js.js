89f12eec9e13ff67c3e6379994308ff73c7eb5ed
Heri-ai <157000679+Heri-ai@users.noreply.github.com>
2024-01-18 15:33:42 +0700

    mini project revou Heri

---
+++ revoU js.js
@@ -0,0 +1,17 @@
+function luas(){
+    var s=document.getElementById('sisi').value;
+    var luas= s*s;
+    document.getElementById('sisi').innerHTML=luas;
+}
+function kel(){
+    var s=document.getElementById('sisi').value;
+    var kel= 4*s;
+    dokument.getElementById('sisi').innerHTML=kel;
+}
+function button() {
+let s=sisi.value;
+let l=s*s;
+output.innerHTML='Luasnya adalah $(l)';    
+}
+
+
